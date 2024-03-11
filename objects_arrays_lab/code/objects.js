be676e54b2c5d14c2a8a830be6e5ea106375793a
Kajanan <Kajanan15i@gmail.com>
2024-03-11 14:50:03 +0000

    completed task 5 objetcs.js

--- objects_arrays_lab/code/objects.js
+++ objects_arrays_lab/code/objects.js
@@ -51,15 +51,19 @@ tortoise.age ++;
 
 dogColourCount = dog.colours.length;
 
-console.log("The dog has " + dogColourCount + " different colours");
+//console.log("The dog has " + dogColourCount + " different colours");
 
 /**
  * Q5. I've just bought a 12-year-old African Grey parrot called Lucky. Create an object for her.
  */
+parrot = {
+  name: "Lucky",
+  age: 12,
+  breed: "African",
+  colours: ["grey"],
+};
 
-parrot = null;
-
-// console.log(parrot);
+console.log(parrot);
 
 /**
  * Q6. Ella's favourite food is dandelion, add this information to her object
